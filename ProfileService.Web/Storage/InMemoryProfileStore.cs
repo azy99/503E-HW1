6feb3ff03e25fd2bf79de0991c4c30d62d346503
Ali Yunes <117394409+azy99@users.noreply.github.com>
2023-02-12 22:44:32 +0200

    Update InMemoryProfileStore UpsertProfile and add DeleteProfile

--- ProfileService.Web/Storage/InMemoryProfileStore.cs
+++ ProfileService.Web/Storage/InMemoryProfileStore.cs
@@ -9,6 +9,14 @@ public class InMemoryProfileStore : IProfileStore
         
     public Task UpsertProfile(Profile profile)
     {
+        if (profile == null ||
+            string.IsNullOrWhiteSpace(profile.Username) ||
+            string.IsNullOrWhiteSpace(profile.FirstName) ||
+            string.IsNullOrWhiteSpace(profile.LastName)
+           )
+        {
+            throw new ArgumentException($"Invalid profile {profile}", nameof(profile));
+        }
         _profiles[profile.Username] = profile;
         return Task.CompletedTask;
     }
@@ -17,5 +25,11 @@ public class InMemoryProfileStore : IProfileStore
     {
         if (!_profiles.ContainsKey(username)) return Task.FromResult<Profile?>(null);
         return Task.FromResult<Profile?>(_profiles[username]);
+    }
+
+    public Task DeleteProfile(string username)
+    {
+        _profiles.Remove(username);
+        return Task.CompletedTask;
     }
 }
